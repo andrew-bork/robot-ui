8cf5b6696ce798482fe56dbef7c87e29fde5adeb
bork <bork.andrew.ryan@gmail.com>
2024-01-13 02:51:12 -0800

    Added robot context

--- src/components/robot-context/robot-context.tsx
+++ src/components/robot-context/robot-context.tsx
@@ -10,6 +10,23 @@ interface Coordinate {
     long: number,
 };
 
+interface Quarternion {
+    w: number,
+    x: number,
+    y: number, 
+    z: number,
+}
+
+interface Vector3 {
+    x: number,
+    y: number, 
+    z: number,
+}
+
+interface Polar {
+    r: number,
+    theta: number,
+};
 
 interface Waypoint {
     location: Coordinate,
@@ -23,41 +40,71 @@ interface Waypoint {
 
 interface RobotTelemetryData {
     drive: {
+        setpoints: {
+            steering_axis_angle: number,
+            steering_angle: number,
+            wheel_speed: number,
+        }
+
+        steering_axis_angle: number,
+        steering_angle: number,
+        wheel_speed: number,
+    },
+    navigation: {
+        air_pressure: number,
+        altitude: number,
         
+        gps_location: Coordinate,
+        heading: number,
+
+        velocity: Vector3,
+        acceleration: Vector3,
+
+        orientation: Quarternion,
+        angular_velocity: Quarternion,
     },
     automony: {
-
-    },
-    science: {
-
-    },
-    system: {
-        ping: 10
-    },
-
-};
-
-interface RobotServerData {
-    drive: {
-        
-    },
-    automony: {
+        lidar: Polar[],
         waypoints: Waypoint[],
     },
     science: {
+        co2_value: number,
+        humidity: number,
+        air_pressure: number,
 
     },
     system: {
-        ping: 10
+        ping: number,
+        battery: number,
+        state: string,
     },
 };
 
+type CommandStatus = Response;
+
+interface Robot {
+    robot: RobotTelemetryData,
+    sendCommand: Promise<CommandStatus>,
+}
 
 
+function sendCommand?() {}
 
+const RobotContext = useContext<Robot>();
 
+export function useRobot() {
+    const {}  = 
+}
 
+export function useDriveData() {
+    
+}
 
+export function useNavigationData() {
+
+}
+
+// export function use
 
 // export const RobotContext = createContext();
 
